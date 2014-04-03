c28239605172577c5e18882d5a5cbf5a47488ac9
Mahmoud El Shaffie <mahmoud.shaffie@marisoftoil.com>
2014-04-03 13:57:40 +0200

    Commit

---
+++ ProcessCoordinatorDevelopmentWS/JMSConsumer/src/main/java/com/marisoft/ziba/cep/jms/MyInputEventListener.java
@@ -0,0 +1,15 @@
+package com.marisoft.ziba.cep.jms;
+
+import java.util.Map;
+
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+
+public class MyInputEventListener implements UpdateListener {
+
+	public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+		 Map<String, Object> event = (Map<String, Object>)newEvents[0].getUnderlying();
+		 System.out.println(event);
+	}
+
+}
